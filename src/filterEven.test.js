3ad3b3aa81d1be42b5fb1c3b9df9f5cc629b6f02
AdamSzabo1 <98642608+AdamSzabo1@users.noreply.github.com>
2024-11-09 12:38:49 +0100

    finished filterEven feature

--- src/filterEven.test.js
+++ src/filterEven.test.js
@@ -1,30 +1,25 @@
 
 
-describe.only('filterEven', () => {
+describe('filterEven', () => {
 
     beforeEach(() => {
         jest.resetModules();
-    });
-
-    test.skip('Can filter [1, 2, 4]', () => {
-
-        const result = filterEven([1, 2, 4]);
-
-        expect(result).toStrictEqual([2, 4]);
     });
 
     test('Can filter [6, 8, 10]', () => {
 
         jest.doMock('./divideNums', () => ({
             ...jest.requireActual('./divideNums'),
-            divideNums: () => ([1, 0])
+            divideNums: jest.fn( () => ([1, 0]) ) 
         }));
 
+        const { divideNums } = require('./divideNums');
         const { filterEven } = require('./filterEven');
 
         const result = filterEven([6, 8, 10]);
 
         expect(result).toStrictEqual([6, 8, 10]);
+        expect(divideNums).toHaveBeenCalledTimes(3);
     });
 
     test('Can filter [3, 5, 7]', () => {
@@ -42,6 +37,20 @@ describe.only('filterEven', () => {
         const result = filterEven([3, 5, 7]);
 
         expect(result).toStrictEqual([]);
+    });
+
+    test('should throw if argument isn\'t an array', () => {
+
+        const { filterEven } = require('./filterEven');
+
+        expect( () => { filterEven(5) } ).toThrow();
+    });
+
+    test('should throw if argument array contains non-number', () => {
+
+        const { filterEven } = require('./filterEven');
+
+        expect( () => { filterEven([ 4, 6, "5", 2 ]) } ).toThrow();
     });
 
 });
